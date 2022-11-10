d7f1b647ccc741b0ec76a19edb37d595e34c7e6c
patiwwb <73791044+patiwwb@users.noreply.github.com>
2022-11-10 18:27:10 +0000

    added notes

--- example2/app/main.py
+++ example2/app/main.py
@@ -13,3 +13,6 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+if __name__ == '__main__':
+    uvicorn.run(app,port = 8000, host="0.0.0.0")
